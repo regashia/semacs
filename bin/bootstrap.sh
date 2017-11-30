26843ac4c308312a666caf7faef88807dbc8980a
Regashia <regashia@gmail.com>
2017-11-30 21:56:57 +0900

    bootstrap で Spacemacs を clone する

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,9 @@
 cd $(dirname "${0}")/..
 
+if [ ! -e ${HOME}/.emacs.d ]; then
+  git clone https://github.com/syl20bnr/spacemacs.git ${HOME}/.emacs.d
+fi
+
 ln -sfv $(pwd)/spacemacs ${HOME}/.spacemacs
 
 cd -
